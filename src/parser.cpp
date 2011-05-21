a1481c50b56611abe102932023e4348d93bb00f1
selius <alexander.nartov@gmail.com>
2011-05-21 11:11:24 +0000

    Added licensing info to each file. Added README and COPYING. Added Doxyfile and doc/ directory.
    git-svn-id: file:///var/svn/ncc/trunk@75 08507c0a-0f99-4e9f-9456-eee6f5b73aae

--- src/parser.cpp
+++ src/parser.cpp
@@ -1,3 +1,21 @@
+/*
+	ncc - Nartov C Compiler
+	Copyright 2010-2011  Alexander Nartov
+
+	ncc is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	ncc is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with ncc.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "parser.h"
 
 #include "optimization.h"
